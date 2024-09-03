c1525bcb423503ba4994e8d016488ce56c9516d0
Tarun Jhamb <tarunjhamb2705@gamil.com>
2024-09-03 12:39:04 +0530

    created signup api route

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,37 @@
+import User from "../models/user.model.js";
+// use bcryptjs full name because bcrypt is going to give error during the deployment.
+import bcryptjs from "bcryptjs";
+
+// we are making this async because singup a user takes time in creating the user in our database. and then we send the response to the user.
+
+export const signup = async (req, res) => {
+    const { username, email, password } = req.body;
+
+    // we are adding extra layer of security, but this is checked by mongoDb as well
+    if (!username || !email || !password || username === '' || email === '' || password === '') {
+        // tested with postman ✔️
+        return res.status(400).json({ message: "All fields are required." });
+    }
+
+    // we don't have to use await here because hashSync already have the await by itself.
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+
+    // because of ES6, if key-value pair name is same we don't have to write them twice.
+    const newUser = new User({
+        username,
+        email,
+        password: hashedPassword,
+    });
+
+    // Now we have to save it inside the database, but we are not going to save the password as it is, so we use bcryptjs for hasing our password.
+    // we are going to use try/catch for proper error handling because without this if we get any error it will display internal not to the client as response.
+
+    try {
+        await newUser.save();
+        res.json("Signup successful");
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+
+}
+
--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -13,6 +15,13 @@ mongoose.connect(process.env.MONGO).then(
 })
 
 const app = express();
+
+// to be able to send json to the backend
+
+app.use(express.json());
+
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
 
 app.listen(3000, ()=>{
     console.log('Server is running on port 3000!')
